81056aef46116d007908007a69a3f46666f587de
VarunUpadhyaya420 <varunupadhyaya2006@gmail.com>
2026-08-15 19:24:27 +0530

    Update Transpose.c

--- Transpose.c
+++ Transpose.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 int main(){
-    int i,j,m,n,temp;
+    int i,j,m,n;
     printf("Enter the number of rows");
     scanf("%d",&m);
-    printf("Enter the number of column");
+    printf("Enter the number of columns");
     scanf("%d",&n);
     int arr[m][n];
-    for(int i=0;i<n;i++){
+    for(i=0;i<n;i++){
         for(j=0;j<m;j++){
             scanf("%d",&arr[i][j]);
         }
     }
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+    printf("Transpose of given array is:\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
             printf("%d ",arr[j][i]);
         }
         printf("\n");
     }
     return 0;
 }
-    
